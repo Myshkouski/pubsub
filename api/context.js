dcb6000ad87ed76b9b15e523f765a31c64c3a14c
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2018-09-04 09:02:35 +0300

    Auto stash before merge of "sandbox" and "pubsub/sandbox"

--- api/context.js
+++ api/context.js
@@ -1,5 +1,97 @@
 module.exports = createContext
 
+const requestUpgradeProto = {
+
+}
+
+function createUpgradeContext() {
+  const ctx = {}
+}
+
+class Context {
+  constructor(request, socket, head) {
+    const ctx = this
+
+    Object.defineProperties(ctx. {
+      request: {
+        get() {
+          return request
+        }
+      },
+      socket: {
+        get() {
+          return socket
+        }
+      },
+      head: {
+        get() {
+          return head
+        }
+      },
+      headers: {
+        get() {
+          return this.request.headers
+        }
+      },
+      responseHeaders: {
+        value: {}
+      }
+    })
+
+    Object.assign(ctx, {
+      get url() {
+        return this.request.url
+      }
+    })
+  }
+}
+
+class UpgradeContext extends Context {
+  constructor(request, socket, head) {
+    super(request, socket, head)
+
+    const ctx = this
+
+    Object.defineProperties(ctx. {
+
+    })
+
+    Object.assign(ctx, {
+      get url() {
+        return this.request.url
+      }
+    })
+  }
+
+  set(header, value) {
+    this.responseHeaders[header] = value
+  }
+}
+
+class MessageContext extends Context {
+  constructor(message, websocket, request, socket, head) {
+    super(request, socket, head)
+
+    const ctx = this
+
+    Object.defineProperties(ctx. {
+      message: {
+        value: message
+      }
+    })
+
+    Object.assign(ctx, {
+      get url() {
+        return this.request.url
+      }
+    })
+  }
+
+  set(header, value) {
+    this.responseHeaders[header] = value
+  }
+}
+
 function createContext(src) {
   const ctx = {}
 
@@ -8,12 +100,12 @@ function createContext(src) {
   })
 
   const obj = Object.assign({}, src)
-  if('_' in obj) {
+  if ('_' in obj) {
     delete obj._
   }
   Object.assign(ctx, obj)
 
-  for(const nonEnumerableKey in ctx._) {
+  for (const nonEnumerableKey in ctx._) {
     Object.defineProperty(ctx, nonEnumerableKey, {
       get() {
         return this._[nonEnumerableKey]
