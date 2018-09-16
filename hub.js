9c8c0a097d6537c39ba1f37688b27aade2337855
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2018-09-16 22:00:49 +0300

    wip

--- hub.js
+++ hub.js
@@ -30,28 +30,67 @@ function _normalizeChannelName(channelName, separator) {
   return channelName
 }
 
+function _publish(subscribers, ...args) {
+  if (subscribers.length) {
+    setImmediate(() => {
+      for (var index in subscribers) {
+        var subscriber = subscribers[index]
+        subscriber.call(undefined, ...args)
+      }
+    })
+  }
+
+  return subscribers.length
+}
+
+class Token {
+  constructor(hub, channelName, subscriber) {
+    this._hub = hub
+    this._channelName = channelName
+    this._subscriber = subscriber
+  }
+
+  get channel() {
+    return this._channelName
+  }
+
+  broadcast(...args) {
+    this._hub.broadcast(this, ...args)
+  }
+
+  unsubscribe() {
+    this._hub.unsubscribe(this)
+  }
+}
+
 class Hub {
+  static normalizeName() {
+    return _normalizeChannelName.apply(this, arguments)
+  }
+
   constructor(options = {}) {
-    this._channelNames = {}
-    this._subscriptions = {}
-    this._muted = {}
-
-    this._separator = options.separator || DEFAULT_SEPARATOR
-    this._linked = options.linked || false
-
-    if (this._linked) {
-      this._parentChannelNames = {}
-      this._childChannelNames = {}
-    }
-
     var hub = this
 
-    hub._ee = []
-    hub._eeChannels = {}
-    hub._eeSubscriptionsCount = []
-    hub._eeListeningEventNames = []
-    hub._eePublishers = []
-    hub._eeSubscribers = {}
+    hub._channelNames = {}
+    hub._subscriptions = {}
+    hub._muted = {}
+
+    hub._separator = options.separator || DEFAULT_SEPARATOR
+    hub._linked = options.linked || false
+
+    if (hub._linked) {
+      hub._parentChannelNames = {}
+      hub._childChannelNames = {}
+    }
+
+    // hub._ee = []
+    // hub._eeChannels = {}
+    // hub._eeSubscriptionsCount = []
+    // hub._eeListeningEventNames = []
+    // hub._eePublishers = []
+    // hub._eeSubscribers = {}
+
+    hub._tokens = {}
   }
 
   create(channelName) {
@@ -103,17 +142,19 @@ class Hub {
 
     hub._channelNames[channelName] = parsedChannelName
     hub._subscriptions[channelName] = []
-    hub._eeChannels[channelName] = []
-    hub._eeSubscribers[channelName] = []
+    // hub._eeChannels[channelName] = []
+    // hub._eeSubscribers[channelName] = []
+
+    hub._tokens[channelName] = []
   }
 
-  remove(channelName, removeChildChannels) {
+  remove(channelName) {
     var hub = this
 
     channelName = _normalizeChannelName(channelName, hub._separator)
     if (channelName in hub._channelNames) {
       var channelNames = [channelName]
-      if (hub._linked && removeChildChannels) {
+      if (hub._linked) {
         var childChannelNames = hub._childChannelNames
         for (var name in childChannelNames) {
           channelNames.push(name)
@@ -122,20 +163,25 @@ class Hub {
 
       for (var nameIndex in channelNames) {
         channelName = channelNames[nameIndex]
-        var eventEmitters = [].concat(hub._eeChannels[channelName])
-        for (var index in eventEmitters) {
-          hub.disconnect(channelName, eventEmitters[index])
+        // var eventEmitters = [].concat(hub._eeChannels[channelName])
+        // for (var index in eventEmitters) {
+        //   hub.disconnect(channelName, eventEmitters[index])
+        // }
+
+        var tokens = [].concat(hub._tokens[channelName])
+        for (var index in tokens) {
+          hub.unsubscribe(token[index])
         }
 
+        delete hub._tokens[channelName]
         delete hub._channelNames[channelName]
         delete hub._subscriptions[channelName]
         if (this._linked) {
           delete hub._childChannelNames[channelName]
           delete hub._parentChannelNames[channelName]
         }
-        delete hub._eeChannels[channelName]
-        delete hub._eeSubscribers[channelName]
-
+        // delete hub._eeChannels[channelName]
+        // delete hub._eeSubscribers[channelName]
       }
     }
 
@@ -149,13 +195,15 @@ class Hub {
     if (channelName in hub._channelNames) {
       var subscribers = hub._subscriptions[channelName]
 
-      if (!~subscribers.indexOf(subscriber)) {
-        subscribers.push(subscriber)
-        return true
-      }
+      const token = new Token(hub, channelName, subscriber)
+
+      subscribers.push(subscriber)
+      hub._tokens[channelName].push(token)
+
+      return token
     }
 
-    return false
+    return null
   }
 
   wait(channelName, subscriber) {
@@ -169,24 +217,52 @@ class Hub {
     return hub.subscribe(channelName, _subscriber)
   }
 
-  unsubscribe(channelName) {
+  unsubscribe(token) {
     var hub = this
 
-    channelName = _normalizeChannelName(channelName, hub._separator)
-    if (channelName in hub._channelNames) {
-      var subscribers = hub._subscriptions[channelName]
-      if (1 in arguments) {
-        var subscriber = arguments[1]
-        return subscribers.splice(subscribers.indexOf(subscriber), 1)
-      } else {
-        return subscribers.splice(index, subscribers.length)
+    if (token._hub === hub) {
+      var channelName = token._channelName
+      if (channelName in hub._channelNames) {
+        var index = hub._subscriptions[channelName].indexOf(token._subscriber)
+        if (~index) {
+          hub._subscriptions[channelName].splice(index, 1)
+          hub._tokens[channelName].splice(index, 1)
+          return true
+        }
       }
     }
 
     return false
   }
 
-  publish(channelName, applyFilter, ...args) {
+  broadcast(token, ...args) {
+    var hub = this
+
+    if (token && (token._hub === hub)) {
+      var channelName = token._channelName
+
+      if (channelName in hub._channelNames) {
+        var index = hub._subscriptions[channelName].indexOf(token._subscriber)
+        if (~index) {
+          var subscribers = [].concat(hub._subscriptions[channelName])
+          subscribers.splice(index, 1)
+
+          if (hub._linked) {
+            var childChannels = hub._childChannelNames[channelName]
+            for (var childChannelName in childChannels) {
+              subscribers = subscribers.concat(hub._subscriptions[childChannelName])
+            }
+          }
+
+          return _publish.call(hub, subscribers, ...args)
+        }
+      }
+    }
+
+    return 0
+  }
+
+  publish(channelName, ...args) {
     var hub = this
 
     channelName = _normalizeChannelName(channelName, hub._separator)
@@ -194,111 +270,16 @@ class Hub {
       var subscribers = [].concat(hub._subscriptions[channelName])
 
       if (hub._linked) {
-        var parentChannels = hub._childChannelNames[channelName]
-        for (var parentChannelName in parentChannels) {
-          subscribers = subscribers.concat(hub._subscriptions[parentChannelName])
+        var childChannels = hub._childChannelNames[channelName]
+        for (var childChannelName in childChannels) {
+          subscribers = subscribers.concat(hub._subscriptions[childChannelName])
         }
       }
 
-      if (subscribers.length) {
-        subscribers = subscribers.filter(subscriber => applyFilter(subscriber))
-        // setImmediate(() => {
-        for (var index in subscribers) {
-          var subscriber = subscribers[index]
-          subscriber.call(undefined, ...args)
-        }
-        // })
-
-        return subscribers.length
-      }
+      return _publish.call(hub, subscribers, ...args)
     }
 
-    return false
-  }
-
-  broadcast(channelName, ...args) {
-    return this.publish(channelName, () => true, ...args)
-  }
-
-  connect(channelName, eventEmitter, eventNames) {
-    eventNames = Object.assign({
-      message: 'message',
-      broadcast: 'broadcast'
-    }, eventNames || {})
-
-    var hub = this
-
-    channelName = _normalizeChannelName(channelName, hub._separator)
-    if (channelName in hub._channelNames) {
-      var eeSubscriber
-
-      var index = hub._eeChannels[channelName].indexOf(eventEmitter)
-
-      if (!~index) {
-        hub._eeChannels[channelName].push(eventEmitter)
-
-        eeSubscriber = function _eeSubscriber(...args) {
-          eventEmitter.emit(eventNames.broadcast, channelName, ...args)
-        }
-      } else {
-        eeSubscriber = hub._eeSubscribers[channelName][index]
-      }
-
-      var hubIndex = hub._ee.indexOf(eventEmitter)
-
-      if (!~hubIndex) {
-        function _eePublisher(channelName, ...args) {
-          channelName = _normalizeChannelName(channelName, hub._separator)
-          if (channelName in hub._channelNames) {
-            var channelIndex = hub._eeChannels[channelName].indexOf(eventEmitter)
-            if (~channelIndex) {
-              var eeSubscriber = hub._eeSubscribers[channelName][channelIndex]
-              hub.publish(channelName, subscriber => subscriber !== eeSubscriber, ...args)
-            }
-          }
-        }
-
-        eventEmitter.on(eventNames.message, _eePublisher)
-
-        hubIndex = hub._ee.length
-
-        hub._ee.push(eventEmitter)
-        hub._eeSubscriptionsCount.push(0)
-        hub._eeListeningEventNames.push(eventNames.message)
-        hub._eePublishers.push(_eePublisher)
-      }
-
-      if (!~hub._subscriptions[channelName].indexOf(eeSubscriber)) {
-        hub._subscriptions[channelName].push(eeSubscriber)
-        hub._eeSubscriptionsCount[hubIndex]++;
-        hub._eeSubscribers[channelName].push(eeSubscriber)
-      }
-    }
-  }
-
-  disconnect(channelName, eventEmitter) {
-    var hub = this
-
-    channelName = _normalizeChannelName(channelName, hub._separator)
-    if (channelName in hub._channelNames) {
-      var hubIndex = hub._ee.indexOf(eventEmitter)
-      if (~hubIndex) {
-        var eeSubscriber = hub._eeSubscribers[channelName][hubIndex]
-        hub._subscriptions[channelName].splice(hub._subscriptions[channelName].indexOf(eeSubscriber), 1)
-        var channelIndex = hub._eeChannels[channelName].indexOf(eventEmitter)
-        hub._eeSubscribers[channelName].splice(channelIndex, 1)
-
-        hub._eeChannels[channelName].splice(channelIndex, 1)
-        if (!--hub._eeSubscriptionsCount[hubIndex]) {
-          var eePublisher = hub._eePublishers[hubIndex]
-          hub._eePublishers.splice(hubIndex, 1)
-          hub._eeSubscriptionsCount.splice(hubIndex, 1)
-          eventEmitter.removeListener(hub._eeListeningEventNames[hubIndex], eePublisher)
-          hub._eeListeningEventNames.splice(hubIndex, 1)
-          hub._ee.splice(hubIndex, 1)
-        }
-      }
-    }
+    return 0
   }
 }
 
